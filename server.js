e654e2a3b5d872e9d6e7a4bebeb954113906577d
juan <juannarcal54@gmail.com>
2026-04-12 20:30:04 -0300

    feat: backend inicial com prisma e postgres

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ import app from './src/app.js';
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`🚀 Servidor rodando!`);
+    console.log(`👉 Acesse: http://localhost:${PORT}`);
 });
